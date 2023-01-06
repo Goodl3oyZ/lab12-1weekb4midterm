e03678aae134a08493381d6caa6e28ce9faff022
Goodl3oyZ <panudetzx2@gmail.com>
2023-01-06 15:31:25 +0700

    12.1sedla

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,50 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+using namespace std;
+string gradestr;
+int grade;
+int graderandom(){
+    return grade=rand()%9+1;
+}
+int main()
+{
+    cout<<"Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    srand(time(0));
+    graderandom();
+    switch(grade){
+        case 1:
+            gradestr="A";
+            break;
+        case 2:
+            gradestr="B+";
+            break;
+        case 3:
+            gradestr="B";
+            break;
+        case 4:
+            gradestr="C+";
+            break;
+        case 5:
+            gradestr="C";
+            break;
+        case 6:
+            gradestr="D+";
+            break;
+        case 7:
+            gradestr="D";
+            break;
+        case 8:
+            gradestr="F";
+            break;
+        case 9:
+            gradestr="W";
+            break;
+
+    }
+    cout<<"You will get "<<gradestr<<" in this 261102.";
+}
